60c76534e553bd980eb5f90ba16ac97d8c30838a
Somto111 <130008689+Somto111@users.noreply.github.com>
2026-04-25 19:09:11 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <section class="bg-black h-vh ">
+        <div class="p-4 flex justify-between items-center  py-10 w-11/12">
+          <img src="./src/assets/Group 214.png" alt="hero" class="w-10 relative bottom-8" />
+          <p className='text-amber-50 items-center text-2xl'>20+ SaaS Waitlist <br/>  Landing Page Examples</p>
+          <img src="./src/assets/Figma.png" alt="figma" className='w-7'/>
+        </div>
+        
+        <img src="./src/assets/Image wrap.png" alt="wrap"  class="w-full px-2 md:w-2xl"/>
+        
+
+      </section>
+    </>
+  )
+}
+
+export default App
