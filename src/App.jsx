e23010f9a318a19d86a9201a39b5cf20dab193e8
Somto111 <130008689+Somto111@users.noreply.github.com>
2026-04-27 13:04:30 +0100

    text edit

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,20 @@ function App() {
 
   return (
     <>
-      <section class="bg-black h-vh ">
-        <div class="p-4 flex justify-between items-center  py-10 w-11/12">
+      <section class="bg-black  ">
+        <div class="p-4 flex justify-between items-center   py-10 w-11/12  ">
           <img src="./src/assets/Group 214.png" alt="hero" class="w-10 relative bottom-8" />
           <p className='text-amber-50 items-center text-2xl'>20+ SaaS Waitlist <br/>  Landing Page Examples</p>
           <img src="./src/assets/Figma.png" alt="figma" className='w-7'/>
         </div>
         
-        <img src="./src/assets/Image wrap.png" alt="wrap"  class="w-full px-2 md:w-2xl"/>
-        
+        <img src="./src/assets/Image wrap.png" alt="wrap"  class="px-2"/>
+      </section>
 
+      <section className='bg-amber-700 bg-linear-to-r from-blue-500 via-indigo-600 to-purple-500 '>
+        <p>my name is John Doe</p>
+        <p>my name is John Doe</p>
+        <p>my name is John Doe</p>
       </section>
     </>
   )
